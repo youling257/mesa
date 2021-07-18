628bf56d12c5727e657c006f76446290128c0f18
youling257 <youling257@gmail.com>
2021-07-19 00:36:27 +0800

    Revert "aco: Fix how p_elect interacts with optimizations."
    This reverts commit 55d57b828f7669a1fd5e4c6c9cc04370a88d31a2.

--- src/amd/compiler/aco_instruction_selection.cpp
+++ src/amd/compiler/aco_instruction_selection.cpp
@@ -8800,11 +8800,7 @@ visit_intrinsic(isel_context* ctx, nir_intrinsic_instr* instr)
       break;
    }
    case nir_intrinsic_elect: {
-      /* p_elect is lowered in aco_insert_exec_mask.
-       * Use exec as an operand so value numbering and the pre-RA optimizer won't recognize
-       * two p_elect with different exec masks as the same.
-       */
-      Temp elected = bld.pseudo(aco_opcode::p_elect, bld.def(bld.lm), Operand(exec, bld.lm));
+      Temp elected = bld.pseudo(aco_opcode::p_elect, bld.def(bld.lm));
       emit_wqm(bld, elected, get_ssa_temp(ctx, &instr->dest.ssa));
       ctx->block->kind |= block_kind_needs_lowering;
       break;
