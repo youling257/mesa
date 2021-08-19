35f8f2fcac159a3832e03fed05b8a2e758501faa
youling257 <youling257@gmail.com>
2021-08-19 13:00:46 +0800

    Revert "panfrost: Rewrite the clear colour packing code"
    This reverts commit b9c095cc2c6874625a50805b7914cca74b8742bb.

--- src/panfrost/lib/pan_util.h
+++ src/panfrost/lib/pan_util.h
@@ -28,10 +28,6 @@
 #ifndef PAN_UTIL_H
 #define PAN_UTIL_H
 
-#include <stdint.h>
-#include <stdbool.h>
-#include "util/format/u_format.h"
-
 #define PAN_DBG_PERF            0x0001
 #define PAN_DBG_TRACE           0x0002
 #define PAN_DBG_DEQP            0x0004
@@ -47,8 +43,6 @@
 #define PAN_DBG_LINEAR          0x1000
 #define PAN_DBG_NO_CACHE        0x2000
 
-struct panfrost_device;
-
 unsigned
 panfrost_translate_swizzle_4(const unsigned char swizzle[4]);
 
@@ -59,9 +53,6 @@ unsigned
 panfrost_format_to_bifrost_blend(const struct panfrost_device *dev,
                                  enum pipe_format format,
                                  bool dithered);
-
-void
-pan_pack_color(uint32_t *packed, const union pipe_color_union *color, enum pipe_format format);
 
 /* Get the last blend shader, for an erratum workaround on v5 */
 
